2dd1e9735e8c36d4e5e92bf513a8e246f9fc46a1
Guillermo Sámano <guillermoasamanol@outlook.com>
2026-03-08 18:53:27 -0600

    Se agrego la seccion About.vue con sus animaciones

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import Hero from './components/Hero.vue';
+import About from './components/About.vue';
 
 </script>
 
 <template>
   <main class="min-h-dvh w-dvw overflow-x-hidden">
     <Hero />
-    <section class="z-0 min-h-dvh bg-blue-500" />
+    <About />
   </main>
 </template>
 
---
+++ src/components/About.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+
+const initClipAnimation = ():void => {
+    const clipAnimation = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#clip',
+            start: 'center center',
+            end: '+=800 center', // Indica que la animación durará 800px de desplazamiento después de alcanzar el centro del trigger,
+            scrub: 0.5, // Sincroniza la animación con el desplazamiento del scroll, con una suavización de 0.5 segundos
+            pin: true, // Indica que el elemento se mantendrá fijo durante la animación
+            pinSpacing: true // Asegura que el espacio del elemento se mantenga durante el pinning
+        }
+    })
+    clipAnimation.to('.mask-clip-path', {
+        width: '100vw',
+        height: '100vh',
+        borderRadius: 0
+    })
+}
+
+onMounted(() => {
+    initClipAnimation()
+})
+
+onUnmounted(() => {
+    ScrollTrigger.getAll().forEach(t => t.kill()); // Se limpian todos los scrollTriggers encontrados
+})
+
+</script>
+
+<template>
+    <div id="about" class="min-h-dvh min-w-dvw">
+        <div class="relative mb-8 mt-36 flex flex-col items-center gap-5">
+            <h2 class="font-general text-sm uppercase md:text-[0.625rem]">Welcome to Zentry</h2>
+            <div class="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared
+            </div>
+            <div class="about-subtext">
+                <p>The Game of Games begins-your life, now an epic MMORPG</p>
+                <p>Zentry unites every player from countless games and platforms</p>
+            </div>
+        </div>
+        <div class="h-dvh w-dvw" id="clip">
+            <div class="mask-clip-path about-image">
+                <img src="/img/about.webp" alt="background" class="absolute left-0 top-0 size-full object-cover">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
